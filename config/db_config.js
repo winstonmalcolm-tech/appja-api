0d381debb6b8afecd1902757f28ea36fb3be2da7
Winston Malcolm <winstionmalcolm77@gmail.com>
2025-02-12 20:18:39 -0500

    fix database connection

--- config/db_config.js
+++ config/db_config.js
@@ -1,7 +1,7 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
 //LOCAL CONNECTION
-// const pool = mysql.createPool({
+// const connection = mysql.createConnection({
 //     host: process.env.MYSQL_HOST,
 //     user: process.env.MYSQL_USER,
 //     database: process.env.MYSQL_DB,
@@ -11,7 +11,7 @@ const mysql = require("mysql2/promise");
 
 const url = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 
-const connection = mysql.createConnection(url)
+const connection = mysql.createConnection(url);
 
 module.exports = connection;
 
