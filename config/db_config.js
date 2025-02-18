f5f4848ff4fbc49127051d7cb715915300fa01f0
Winston Malcolm <winstionmalcolm77@gmail.com>
2025-02-18 14:35:28 -0500

    add config

--- config/db_config.js
+++ config/db_config.js
@@ -9,15 +9,16 @@ const mysql = require("mysql2/promise");
 // });
 
 
-<<<<<<< HEAD
 const url = `${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
-=======
-const url = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQL_DATABASE}`;
->>>>>>> fb0c2e05ecbbb8eae062ebeed15d9451726b8738
 
 // const connection = mysql.createConnection(url);
 
-const connection = mysql.createPool(url);
+const connection = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
+});
 
 module.exports = connection;
 
