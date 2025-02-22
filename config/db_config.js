02f25407e9d165f7fbb861a6bb453732d1397387
Winston Malcolm <101194070+winstonmalcolm-tech@users.noreply.github.com>
2025-02-22 16:32:38 -0500

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -9,8 +9,8 @@ const mysql = require("mysql2/promise");
 // });
 
 
-const url = `jdbc:mysql://${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
-
+//const url = `jdbc:mysql://${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+const url = `mysql://avnadmin:${process.env.MYSQLPASSWORD}@mysql-11289512-jobboard-97.g.aivencloud.com:27784/defaultdb?ssl-mode=REQUIRED`;
 // const connection = mysql.createConnection(url);
 
 const connection = mysql.createPool(url);
