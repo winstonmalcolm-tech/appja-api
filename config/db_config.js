1b15ff8c9d03b7a2881426d9f4c19f362d44afe8
Winston Malcolm <winstionmalcolm77@gmail.com>
2025-03-01 16:00:18 -0500

    resolve database issue

--- config/db_config.js
+++ config/db_config.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2/promise");
 
 
-const url = `mysql://avnadmin:${process.env.MYSQLPASSWORD}@mysql-11289512-jobboard-97.g.aivencloud.com:27784/defaultdb?ssl-mode=REQUIRED`;
+const url = `mysql://avnadmin:${process.env.MYSQLPASSWORD}@mysql-11289512-jobboard-97.g.aivencloud.com:27784/appja_db?ssl-mode=REQUIRED`;
 
 const connection = mysql.createPool(url);
 
